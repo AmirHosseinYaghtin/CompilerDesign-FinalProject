1523d072dcce779947d6913b5cf378606881b4b8
Borna <b.maherani@hotmail.com>
2024-12-02 19:38:41 +0330

    Lexer: Borna

---
+++ src/Lexer.cpp
@@ -0,0 +1,405 @@
+#include "Lexer.h"
+#include "llvm/Support/raw_ostream.h"
+
+// classifying characters
+namespace charinfo
+{
+    // ignore whitespaces
+    LLVM_READNONE inline bool isWhitespace(char c)
+    {
+        return (c == ' ' || c == '\t' || c == '\f' || c == '\v' || c == '\r' || c == '\n');
+    }
+
+    LLVM_READNONE inline bool isDigit(char c)
+    {
+        return (c >= '0' && c <= '9');
+    }
+
+    LLVM_READNONE inline bool isLetter(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    LLVM_READNONE inline bool isSpecialCharacter(char c)
+    {
+         return (c == '=' || c == '+' || c == '-' || c == '*' || c == '/' || c == '!' ||
+         c == '>' || c == '<' || c == '(' || c == ')' || c == '{' || c == '}'|| c == ',' ||
+         c == ';' || c == '%' || c == '^' || c == '[' || c == ']' || c == '?' || c == ':') ;
+    }
+
+    LLVM_READNONE inline bool isHash(char c)
+    {
+        return (c == '#');
+    }
+
+    LLVM_READNONE inline bool isDot(char c)
+    {
+        return (c == '.')
+    }
+    LLVM_READNONE inline bool isUnderLine(char c) 
+    {
+        return (c == '_');
+    }
+}
+
+void Lexer::next(Token &token)
+{
+    while (*BufferPtr && charinfo::isWhitespace(*BufferPtr)) 
+    {
+        ++BufferPtr;
+    }
+    // make sure we didn't reach the end of input
+    if (!*BufferPtr)
+    {
+        token.Kind = Token::eoi;
+        return;
+    }
+    // collect characters and check for keywords or ident
+    if (charinfo::isLetter(*BufferPtr))
+    {
+        const char *end = BufferPtr + 1;
+
+        while (charinfo::isLetter(*end) || charinfo::isDigit(*end) || charinfo::isUnderLine(*end))
+            ++end;
+            
+        llvm::StringRef Name(BufferPtr, end - BufferPtr);
+        Token::TokenKind kind;
+        if (Name == "int")
+            kind = Token::KW_int;
+        else if (Name == "bool")
+            kind = Token::KW_bool;
+        else if (Name == "float")
+            kind = Token::KW_float;
+        else if (Name == "var")
+            kind = Token::KW_var;
+        else if (Name == "do")
+            kind = Token::KW_do;
+        else if (Name == "print")
+            kind = Token::KW_print;
+        else if (Name == "while")
+            kind = Token::KW_while;
+        else if (Name == "for")
+            kind = Token::KW_for;
+        else if (Name == "if")
+            kind = Token::KW_if;
+        else if (Name == "case")
+            kind = Token::KW_case;
+        else if (Name == "switch")
+            kind = Token::KW_switch;
+        else if (Name == "const")
+            kind = Token::KW_const;
+        else if (Name == "continue")
+            kind = Token::KW_continue;
+        else if (Name == "default")
+            kind = Token::KW_default;
+        else if (Name == "break")
+            kind = Token::KW_break;
+        else if (Name == "else")
+            kind = Token::KW_else;
+        else if (Name == "true")
+            kind = Token::KW_true;
+        else if (Name == "false")
+            kind = Token::KW_false;
+        else if (Name == "and")
+            kind = Token::KW_and;
+        else if (Name == "or")
+            kind = Token::KW_or;
+        else if (Name == "xor")
+            kind = Token::KW_xor;
+        else if (Name == "min")
+            kind = Token::Func_min;
+        else if (Name == "max")
+            kind = Token::Func_max;
+        else if (Name == "mean")
+            kind = Token::Func_mean;
+        else if (Name == "sqrtN")
+            kind = Token::Func_sqrtN;
+        else
+            kind = Token::ident;
+        // generate the token
+        formToken(token, end, kind);
+        return;
+    }
+
+    else if(charinfo::isHash(*BufferPtr))
+    {
+        const char *end = BufferPtr + 1;
+        while (charinfo::isLetter(*end)) 
+            ++end;
+        llvm::StringRef Name(BufferPtr, end - BufferPtr);
+        Token::TokenKind kind;
+        if(Name == "#define")
+        {
+            kind = Token::KW_define;
+            formToken(token,end,kind);
+        } 
+        else formToken(token, BufferPtr + 1, Token::unknown);
+        return;     
+    }
+
+    else if (charinfo::isDigit(*BufferPtr) || (charinfo::isDot(*BufferPtr) && charinfo::isDigit(*BufferPtr + 1)))
+    { // check for numbers
+        const char *end = BufferPtr;
+        bool isFloatNum = false;
+
+        while (charinfo::isDigit(*end))
+            ++end;
+
+        if (charinfo::isDot(*end))
+        {
+            isFloatNum = (charinfo::isDigit(*end - 1) || charinfo::isDigit(*end + 1)) ? true : false; 
+            ++end;
+            while (charinfo::isDigit(*end))
+                ++end;
+
+            if(isFloatNum) formToken(token, end, Token::float_number);
+            else formToken(token, end, Token::unknown);
+        }
+
+        else formToken(token, end, Token::int_number);
+        return;
+    }
+
+    else if (charinfo::isSpecialCharacter(*BufferPtr)) {
+        const char *endWithOneLetter = BufferPtr + 1;
+        const char *endWithTwoLetter = BufferPtr + 2;
+        const char *end;
+        llvm::StringRef NameWithOneLetter(BufferPtr, endWithOneLetter - BufferPtr);
+        llvm::StringRef NameWithTwoLetter(BufferPtr, endWithTwoLetter - BufferPtr);
+        Token::TokenKind kind;
+        bool isFound = false;
+        if (NameWithTwoLetter == "==")
+        {
+            kind = Token::eq;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithOneLetter == "=") 
+        {
+            kind = Token::assign;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithTwoLetter == "!=")
+        {
+            kind = Token::neq;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "-(")
+        {
+            kind = Token::minus_paren;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithTwoLetter == "+=")
+        {
+            kind = Token::plus_assign;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "-=")
+        {
+            kind = Token::minus_assign;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "*=")
+        {
+            kind = Token::star_assign;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "*/")
+        {
+            kind = Token::end_comment;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithTwoLetter == "/=")
+        {
+            kind = Token::slash_assign;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "%=")
+        {
+            kind = Token::mod_assign;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithTwoLetter == "/*")
+        {
+            kind = Token::start_comment;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithTwoLetter == ">=")
+        {
+            kind = Token::gte;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "<=")
+        {
+            kind = Token::lte;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "++")
+        {
+            kind = Token::plus_plus;
+            isFound = true;
+            end = endWithTwoLetter;
+        } 
+        else if (NameWithTwoLetter == "--")
+        {
+            kind = Token::minus_minus;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithOneLetter == "+")
+        {
+            kind = Token::plus;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == "-")
+        {
+            kind = Token::minus;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == "*")
+        {
+            kind = Token::star;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "/")
+        {
+            kind = Token::slash;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithTwoLetter == "//")
+        {
+            kind = Token::oneLine_comment;
+            isFound = true;
+            end = endWithTwoLetter;
+        }
+        else if (NameWithOneLetter == "+")
+        {
+            kind = Token::plus;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == ">")
+        {
+            kind = Token::gt;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "<")
+        {
+            kind = Token::lt;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "(")
+        {
+            kind = Token::l_paren;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == ")")
+        {
+            kind = Token::r_paren;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "{")
+        {
+            kind = Token::l_brace;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "}")
+        {
+            kind = Token::r_brace;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == "[")
+        {
+            kind = Token::l_bracket;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == "]")
+        {
+            kind = Token::r_bracket;
+            isFound = true;
+            end = endWithOneLetter;
+        }  
+        else if (NameWithOneLetter == ";")
+        {
+            kind = Token::semicolon;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == "?")
+        {
+            kind = Token::qMark;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        else if (NameWithOneLetter == ":")
+        {
+            kind = Token::colon;
+            isFound = true;
+            end = endWithOneLetter;
+        }     
+        else if (NameWithOneLetter == ",")
+        {
+            kind = Token::comma;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "%")
+        {
+            kind = Token::mod;
+            isFound = true;
+            end = endWithOneLetter;
+        } 
+        else if (NameWithOneLetter == "^")
+        {
+            kind = Token::exp;
+            isFound = true;
+            end = endWithOneLetter;
+        }
+        
+        // generate the token
+        if (isFound) formToken(token, end, kind);
+        else formToken(token, BufferPtr + 1, Token::unknown);
+        return;
+    }
+    else
+    {
+        formToken(token, BufferPtr + 1, Token::unknown); 
+        return;         
+    }
+    return;
+}
+
+void Lexer::setBufferPtr(const char *buffer){
+    BufferPtr = buffer;
+}
+
+void Lexer::formToken(Token &Tok, const char *TokEnd,
+                      Token::TokenKind Kind)
+{
+    Tok.Kind = Kind;
+    Tok.Text = llvm::StringRef(BufferPtr, TokEnd - BufferPtr);
+    BufferPtr = TokEnd;
+}
---
+++ src/Lexer.h
@@ -0,0 +1,123 @@
+#ifndef LEXER_H // conditional compilations(checks whether a macro is not defined)
+#define LEXER_H
+
+#include "llvm/ADT/StringRef.h"        // encapsulates a pointer to a C string and its length
+#include "llvm/Support/MemoryBuffer.h" // read-only access to a block of memory, filled with the content of a file
+
+class Lexer;
+
+class Token
+{
+    friend class Lexer; // Lexer can access private and protected members of Token
+
+public:
+    enum TokenKind : unsigned short
+    {
+        eoi,            // end of input
+        unknown,        // in case of error at the lexical level
+        ident,          // identifier
+        int_number,     // integer literal
+        float_number,   // float literal
+        assign,         // =
+        colon,          // :
+        qMark,          // ?
+        minus_assign,   // -=
+        plus_assign,    // +=
+        star_assign,    // *=
+        slash_assign,   // /=
+        mod_assign,     // %=
+        eq,             // ==
+        neq,            // !=
+        gt,             // >
+        lt,             // <
+        gte,            // >=
+        lte,            // <=
+        plus_plus,      // ++
+        minus_minus,    // --
+        oneLine_comment  // "//""
+        start_comment,  // /*
+        end_comment,    // */
+        comma,          // ,
+        semicolon,      // ;
+        plus,           // +
+        minus,          // -
+        star,           // *
+        slash,          // /
+        mod,            // %
+        exp,            // ^
+        l_paren,        // (
+        minus_paren,    // -(
+        r_paren,        // )
+        l_brace,        // {
+        r_brace,        // }
+        l_bracket,      // [
+        r_bracket,      // ]
+        Func_min,       // min()
+        Func_max,       // max()
+        Func_mean,       // mean()
+        Func_sqrtN,       // sqrtN()
+        KW_define,      // #define
+        KW_const,        // const
+        KW_int,         // int
+        KW_float,        // float
+        KW_var,          // var
+        KW_bool,        // bool
+        KW_true,        // true
+        KW_false,       // false
+        KW_if,          // if
+        KW_switch,      // switch
+        KW_case,        // case
+        KW_break,       // break
+        KW_continue,    // continue
+        KW_default,     // default
+        KW_do,          // do
+        KW_else,        // else
+        KW_while,       // while
+        KW_not,         // not
+        KW_in,          // in
+        KW_for,         // for
+        KW_and,         // and
+        KW_or,          // or
+        KW_xor,         // xor
+        KW_print        // print
+    };
+
+private:
+    TokenKind Kind;
+    llvm::StringRef Text; // points to the start of the text of the token
+
+public:
+    TokenKind getKind() const { return Kind; }
+    llvm::StringRef getText() const { return Text; }
+
+    // to test if the token is of a certain kind
+    bool is(TokenKind K) const { return Kind == K; }
+    bool isOneOf(TokenKind K1, TokenKind K2) const
+    {
+        return is(K1) || is(K2);
+    }
+    template <typename... Ts>
+    bool isOneOf(TokenKind K1, TokenKind K2, Ts... Ks)
+        const { return is(K1) || isOneOf(K2, Ks...); }
+};
+
+class Lexer
+{
+    const char *BufferStart; // pointer to the beginning of the input
+    const char *BufferPtr;   // pointer to the next unprocessed character
+
+public:
+    Lexer(const llvm::StringRef &Buffer)
+    {
+        BufferStart = Buffer.begin();
+        BufferPtr = BufferStart;
+    }
+
+    void next(Token &token); // return the next token
+    void setBufferPtr(const char* buffer);
+    const char* getBuffer(){return BufferPtr;};
+
+private:
+    void formToken(Token &Result, const char *TokEnd, Token::TokenKind Kind);
+};
+#endif
